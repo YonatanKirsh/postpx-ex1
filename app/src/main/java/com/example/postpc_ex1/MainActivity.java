11be9afd01e71404b9ea17a769ea2d83a9e10104
Yonatan Kirshenbaum <t-yokirs@microsoft.com>
2020-04-22 23:33:10 +0300

    v0

--- app/src/main/java/com/example/postpc_ex1/MainActivity.java
+++ app/src/main/java/com/example/postpc_ex1/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.postpc_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textView = findViewById(R.id.textView1);
+        this.editText = findViewById(R.id.editText1);
+        Button button = findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText());
+                editText.setText("");
+            }
+        });
+
     }
+
 }
